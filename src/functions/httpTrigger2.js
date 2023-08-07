cf1bf6dcf3e9668ec9a7a881496c7dc27ecbcce9
Grigorii Horos <horosgrisa@gmail.com>
2023-08-07 15:06:58 +0300

    Add

--- src/functions/httpTrigger2.js
+++ src/functions/httpTrigger2.js
@@ -8,6 +8,6 @@ app.http("httpTrigger2", {
 
     const name = request.query.get("name") || (await request.text()) || "world";
 
-    return { body: `Hello, ${name}!` };
+    return { body: `Helloq, ${name}!` };
   },
 });
